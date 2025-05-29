61a065a315171c285bce3fd3c9ed6b199e6b436e
VietHa-02 <habest2k2@gmail.com>
2025-05-29 19:40:26 +0700

    Lưu lại thay đổi trước khi cập nhật từ main

--- src/main/java/com/example/storesports/core/admin/coupon/payload/CouponResponse.java
+++ src/main/java/com/example/storesports/core/admin/coupon/payload/CouponResponse.java
@@ -2,13 +2,17 @@ package com.example.storesports.core.admin.coupon.payload;
 
 import lombok.Data;
 
+import java.time.LocalDateTime;
 import java.util.Date;
 
 @Data
 public class CouponResponse {
     private Long id;
     private String code;
+    private Integer quantity;
     private Double discountAmount;
-    private Date expirationDate;
+    private LocalDateTime expirationDate;
+    private LocalDateTime statDate;
+    private Boolean status;
 
 }
