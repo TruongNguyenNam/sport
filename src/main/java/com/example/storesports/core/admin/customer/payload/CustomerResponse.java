63ce52b034d43abeb7a324b4cdbea09d9290a6e0
VietHa-02 <habest2k2@gmail.com>
2025-07-16 18:56:56 +0700

    commit truoc khi pull

--- src/main/java/com/example/storesports/core/admin/customer/payload/CustomerResponse.java
+++ src/main/java/com/example/storesports/core/admin/customer/payload/CustomerResponse.java
@@ -1,7 +1,10 @@
 package com.example.storesports.core.admin.customer.payload;
 
+import com.example.storesports.core.admin.address.payload.AddressResponse;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.List;
 
 @Data
 @NoArgsConstructor
@@ -11,14 +14,7 @@ public class CustomerResponse {
     private String username;
     private String phoneNumber;
     private String role;
-    private String addressStreet; // ok
-    private String addressWard; // Phường (Phường Phúc Đồng)
-    private String addressCity;  // ok
-    private String addressState; // ok
-    private String addressCountry;
-    private String AddressZipcode;
-    private String addressDistrict; // Quận/Huyện (Huyện Vĩnh Tuy)
-    private String addressProvince; // Tỉnh (Quận Long Biên)
     private boolean isActive;
     private String gender;
+    private List<AddressResponse> addresses;
 }
